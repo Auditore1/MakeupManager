d8a509a0301327dd8b655dbb3cf341df4879ee55
Thyago Sanches <thiago.afonso.sanches@hotmail.com>
2021-02-10 04:37:37 -0300

    Acrescentando Slider mobile

--- js.js
+++ js.js
@@ -5,15 +5,23 @@
       direction: 'left'
     });
   });
+//acima é  botão flutuante Materializze.css :v não sei se vou deixar neh mas dx ae
+function addback(foto){
+  document.getElementById("mold").style.backgroundImage = "URL("+foto+")";
+}
+
+
+  //abaixo é a animação
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
+  
   }
   function setfilho(){
   const quadro = document.querySelector("ul.ula");	
   
-  for (let i = 0; i < 50; i++){
+  for (let i = 0; i < 20; i++){
            const li = document.createElement("li");
            //const random = (min,max) => Math.random()*(max - min) + min;
            const size = Math.floor(Math.random() * (90 - 10) ) + 10;;
@@ -22,11 +30,21 @@
            li.style.height = size+'px';
            li.style.bottom = '-10000';
            li.style.left = position+'%';
-           li.style.opacity = Math.random() * (0.9 - 0.4)+0.4;
-           li.style.animationDelay = getRandomInt(10,200)+'s';
-           li.style.animationDuration = getRandomInt(4, 30)+'s';
+           li.style.opacity = Math.random() * (0.2 - 0.1)+0.1;
+           li.style.animationDelay = getRandomInt(5,30)+'s';
+           li.style.animationDuration = getRandomInt(20, 60)+'s';
+           li.style.borderRadius = "50%";
+          li.style.backgroundImage = "URL(img/sl/AndyCL"+getRandomInt(1,7)+".png)";
+          li.style.backgroundSize = "cover";
            li.style.animationTimingFunction = 'cubic-bezier(${Math.random()},${Math.random()},${Math.random()},${Math.random()})';
            quadro.appendChild(li);
   }
   }
   document.addEventListener("DOMContentLoaded", setfilho);
+  document.addEventListener("DOMContentLoaded",resetimg);
+ function resetimg(){
+    // img/sl/name.tipo
+    img = document.getElementById("imgsl");
+    img.style.backgroundImage = "URL(img/sl/AndyCL"+getRandomInt(1,7)+".png)";
+    setTimeout(resetimg, 5000)
+  }
